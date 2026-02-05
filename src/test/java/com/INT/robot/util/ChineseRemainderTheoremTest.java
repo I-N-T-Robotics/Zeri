0f20e76fdf6aab17cfbaa45a8d1fb79c72021d3b
Tmanxyz <tylerlin408@gmail.com>
2026-02-04 21:53:33 -0500

    testing

---
+++ src/test/java/com/INT/robot/util/ChineseRemainderTheoremTest.java
@@ -0,0 +1,14 @@
+package com.INT.robot.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test ;
+
+class ChineseRemainderTheoremTest
+{
+    @Test 
+    void testSimpleRatio()
+    {
+        // var crm = new ChineseRemainderTheorem(10., 20. ) ;
+        Assertions.assertEquals( 1, 2, "failing test" ) ;
+    }
+}
